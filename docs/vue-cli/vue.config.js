43aa77b1412d2533180131ccb383c30ed15fe72f
zhmhbest <zhmhbest@gmail.com>
2021-12-29 11:37:43 +0000

    CDN资源

--- docs/vue-cli/vue.config.js
+++ docs/vue-cli/vue.config.js
@@ -1,32 +1,85 @@
-const BootCDN = 'https://cdn.bootcdn.net/ajax/libs';
+// https://cli.vuejs.org/config/#pages
+// du -h --max-depth=1 ./dist
+
+
+/**
+ * 获取CDN资源地址
+ * @param {string} lib
+ * @param {string} ver
+ * @param {string} [file]
+ * @return {string}
+ */
+ const cdnUrl = (lib, ver, file) => {
+    // 【bootcdn】 https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js
+    // const cdnPath = 'https://cdn.bootcdn.net/ajax/libs';
+    // file = file || `${lib}.min.js`;
+    // return `${cdnPath}/${lib}/${ver}/${file}`;
+
+    // 【jsdelivr】 https://cdn.jsdelivr.net/npm/axios@0.21.1/dist/axios.min.js
+    const cdnPath = 'https://cdn.jsdelivr.net/npm';
+    file = file || `dist/${lib}.min.js`;
+    return `${cdnPath}/${lib}@${ver}/${file}`;
+};
+
+
+/**
+ * CDN资源表
+ */
 const cdnResources = [
     {
         moduleName: 'axios',
         globalName: 'axios',
-        js: [`${BootCDN}/axios/0.21.1/axios.min.js`]
+        js: [cdnUrl('axios', '0.21.1')]
     },
     {
         moduleName: 'vue',
         globalName: 'Vue',
-        js: [`${BootCDN}/vue/2.6.11/vue.min.js`]
+        js: [cdnUrl('vue', '2.6.11')]
     },
     {
         moduleName: 'vuex',
         globalName: 'Vuex',
-        js: [`${BootCDN}/vuex/3.5.1/vuex.min.js`]
+        js: [cdnUrl('vuex', '3.5.1')]
     },
     {
         moduleName: 'vue-router',
         globalName: 'VueRouter',
-        js: [`${BootCDN}/vue-router/3.2.0/vue-router.min.js`]
-    }
+        js: [cdnUrl('vue-router', '3.2.0')]
+    },
+    {
+        moduleName: 'moment',
+        globalName: 'moment',
+        js: [
+            cdnUrl('moment', '2.21.0', 'moment.min.js'),
+            cdnUrl('moment', '2.21.0', 'locale/zh-cn.js')
+        ],
+    },
+    {
+        moduleName: 'ant-design-vue',
+        globalName: 'antd',
+        js: [cdnUrl('ant-design-vue', '1.7.6', 'dist/antd.min.js')],
+        css: [cdnUrl('ant-design-vue', '1.7.6', 'dist/antd.min.css')]
+    },
 ];
-const cdnExternals = (function (rs) {
-    for (let r of rs) { this[r.moduleName] = r.globalName; } return this;
-}).call({}, cdnResources);
+
+
+/**
+ * CDN资源替换表
+ */
+const cdnExternals = (() => {
+    let obj = {/* moduleName: globalName */ };
+    for (let res of cdnResources) {
+        obj[res.moduleName] = res.globalName;
+    }
+    return obj;
+})();
+console.log(cdnResources);
 console.log(cdnExternals);
 
 
+/**
+ * exports
+ */
 module.exports = {
     // publicPath: '/',
     configureWebpack: {
